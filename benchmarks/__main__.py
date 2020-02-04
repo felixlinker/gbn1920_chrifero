067ad6906b9c60035cdd1ffff1f41bde68f44533
felixlinker <linkerfelix@gmail.com>
2020-02-04 13:38:56 +0100

    benchmarks is a module

---
+++ benchmarks/__main__.py
@@ -0,0 +1,19 @@
+import argparse
+import timeit
+
+libraries = {
+    'multivitamin': {
+        'stmt': 'from bm_multivitamin import run; run()',
+        'setup': 'from bm_multivitamin import setup; setup(args.inputs)',
+    },
+}
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--inputs', '-i', nargs='+')
+parser.add_argument('--library', '-l', choices=libraries.keys())
+parser.add_argument('--number', '-n', type=int, default=20)
+args = parser.parse_args()
+
+duration = timeit.timeit(globals=globals(), **
+                        libraries[args.library], number=args.number)
+print(duration)
