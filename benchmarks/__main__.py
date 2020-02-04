a9533ff3e180c3b6461c672ba08424977291dc2e
felixlinker <linkerfelix@gmail.com>
2020-02-04 14:12:55 +0100

    Allow to benchmark multiple numbers of graphs

--- benchmarks/__main__.py
+++ benchmarks/__main__.py
@@ -1,10 +1,12 @@
 import argparse
 import timeit
+from glob import glob
+from itertools import chain
 
 libraries = {
     'multivitamin': {
         'stmt': 'from bm_multivitamin import run; run()',
-        'setup': 'from bm_multivitamin import setup; setup(args.inputs)',
+        'setup': 'from bm_multivitamin import setup; setup(inputs[:slice_to])',
     },
 }
 
@@ -12,8 +14,18 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--inputs', '-i', nargs='+')
 parser.add_argument('--library', '-l', choices=libraries.keys())
 parser.add_argument('--number', '-n', type=int, default=20)
+parser.add_argument('--asc', '-a', action='store_true')
 args = parser.parse_args()
 
-duration = timeit.timeit(globals=globals(), **
-                        libraries[args.library], number=args.number)
-print(duration)
+def run(i):
+    global slice_to
+    slice_to = i + 1
+    return timeit.timeit(globals=globals(), number=args.number,
+                         **libraries[args.library])
+
+inputs = list(chain.from_iterable(map(glob, args.inputs)))
+durations = map(
+    run,
+    range(1 if args.asc else len(inputs) - 1, len(inputs))
+)
+print(list(durations))
