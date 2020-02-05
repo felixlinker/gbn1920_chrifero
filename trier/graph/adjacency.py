5cd1d166950a8bb06b98ed3ae6ee766a5f9dc669
felixlinker <linkerfelix@gmail.com>
2020-02-05 12:19:59 +0100

    Add adjacency matrix graph stub

---
+++ trier/graph/adjacency.py
@@ -0,0 +1,16 @@
+import numpy
+
+class AdjacencyGraph:
+    def __init__(self, graph=None, matrix=None, edge_labels=None, node_labels=None):
+        self.adj_matrix = numpy.array([])
+        self.edge_labels = numpy.array([])
+        self.node_labels = numpy.array([])
+
+    def decompose(self):
+        pass
+
+    def remove_node(self, node_index):
+        pass
+
+    def to_multivitamin(self):
+        pass
