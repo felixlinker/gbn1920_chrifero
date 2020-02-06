b8e431b071803bb6a961b3403589a4dee0da6d57
felixlinker <linkerfelix@gmail.com>
2020-02-06 14:40:15 +0100

    Add adjacency graph export to multivitamin

--- trier/graph/adjacency.py
+++ trier/graph/adjacency.py
@@ -1,5 +1,8 @@
 import numpy
 from multivitamin.basic.node import Node
+from multivitamin.basic.edge import Edge
+from multivitamin.basic.graph import Graph
+from itertools import chain
 
 
 class AdjacencyGraph:
@@ -46,4 +49,45 @@ class AdjacencyGraph:
         pass
 
     def to_multivitamin(self):
-        pass
+        def t2node(t):
+            i, ls = t
+            return Node(str(i), ls)
+        # Create multivitamin nodes with label and index as id
+        nodes = list(
+            map(t2node, zip(range(0, len(self.node_labels)), self.node_labels)))
+
+        # Indices for each adjecency matrix column
+        indices = range(0, self.adj_matrix.shape[1])
+        # Use the adjecency matrix row as selector for the indices
+        def r2edges(r): return [ i for mask, i in zip(r, indices) if mask ]
+        # list of lists of connected node ids at node index
+        nodei_2_index = map(r2edges, self.adj_matrix)
+
+        def r2labels(r): return [ s for s in r if s ]
+        # list of lists of labels at node index
+        nodei_2_label = map(r2labels, self.edge_labels)
+
+        # Put labels to respective node indices
+        nodei_2_index_label = map(lambda t: zip(
+            t[0], t[1]), zip(nodei_2_index, nodei_2_label))
+
+        def mappingt2edgest(t):
+            i, node_ilts = t
+            from_node = nodes[i]
+
+            def ilt2edge(t):
+                index, label = t
+                return Edge(from_node, nodes[int(index)], label)
+            return (from_node, list(map(ilt2edge, node_ilts)))
+        # map from node to edges involving node
+        node_2_edges = dict(
+            map(mappingt2edgest, zip(indices, nodei_2_index_label)))
+        for node, edges in node_2_edges.items():
+            # Save node2 as neighbor as node (which is key) was used as node1 in
+            # constructor above
+            node.neighbours = set(map(lambda e: e.node2, edges))
+
+        return Graph(id=self.id, nodes=set(node_2_edges.keys()),
+                     edges=set(chain.from_iterable(node_2_edges.values())),
+                     nodes_labelled=True, edges_labelled=True,
+                     is_directed=False)
