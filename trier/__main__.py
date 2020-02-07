e3b47de0026366014d37b7b4d38c97befbf5edc3
felixlinker <linkerfelix@gmail.com>
2020-02-07 11:38:32 +0100

    Bootstrap code guided aligning

--- trier/__main__.py
+++ trier/__main__.py
@@ -2,8 +2,10 @@ from argparse import ArgumentParser
 from glob import glob
 from itertools import chain
 from multivitamin.utils.parser import parse_graph
+from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
 
 from trier.scoring import Symmetric
+from trier.aligning import GuidedAligning
 
 scorers = dict(map(lambda c: (c.__name__, c), [Symmetric]))
 
@@ -16,5 +18,9 @@ args = parser.parse_args()
 inputs = chain.from_iterable(map(glob, args.inputs))
 graphs = map(parse_graph, inputs)
 scorer = scorers[args.scorer](list(graphs))
-
-print(scorer)
+scorer.calc_scoring()
+tree_constructor = DistanceTreeConstructor()
+tree = tree_constructor.upgma(scorer.get_scoring())
+aligner = GuidedAligning(graphs, tree)
+aligning = aligner.calc_aligning()
+print(aligning)
---
+++ trier/aligning.py
@@ -0,0 +1,6 @@
+class GuidedAligning:
+    def __init__(self, graph_list, guide_tree):
+        pass
+
+    def calc_aligning(self):
+        pass
