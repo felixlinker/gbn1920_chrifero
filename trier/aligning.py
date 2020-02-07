702f2ce71f7d04b358fd42e460340ecfe39e130f
roman-at-github <roman.matern@yahoo.de>
2020-02-07 16:08:36 +0100

    Searching for Alignment Pairs with REGEX

--- trier/aligning.py
+++ trier/aligning.py
@@ -1,5 +1,13 @@
 from Bio import Phylo
 from io import StringIO
+import re
+
+def calc(tree):
+    root = tree.root
+    return rec(root.sibling_left, root.sibling_right)
+
+def rec(s1, s2):
+    if 
 
 
 class GuidedAligning:
@@ -14,4 +22,12 @@ class GuidedAligning:
         Phylo.write(self.guide_tree, handle, "newick")
         gt_newickformat = handle.getvalue()
 
+        # clean up useless information in newick-format:
+        newick = (re.sub(":0.00000", "", gt_newickformat))
+
+        pairs = (re.findall(r"\([^,()]+\,[^,()]+\)", newick))
+
+        # interpret newickformat as alignment-guide
+        for g in self.graph_list:
+            if g
         pass
