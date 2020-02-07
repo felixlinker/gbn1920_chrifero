b120065dccae09097c39e2dbefd94da88170c77f
roman-at-github <roman.matern@yahoo.de>
2020-02-07 14:07:29 +0100

    Converting Tree into newick-format

--- trier/aligning.py
+++ trier/aligning.py
@@ -1,5 +1,14 @@
+from Bio import Phylo
+from io import StringIO
+
 class GuidedAligning:
     def __init__(self, graph_list, guide_tree):
+        # convert tree into newick-format
+        handle = StringIO()
+        Phylo.write(guide_tree, handle, "newick")
+        gt_newickformat = handle.getvalue()
+
+
         pass
 
     def calc_aligning(self):
