6f908c626ee488f325de3ece5d6e87b1ef19791f
roman-at-github <roman.matern@yahoo.de>
2020-02-07 14:27:02 +0100

    __init__ finished

--- trier/aligning.py
+++ trier/aligning.py
@@ -1,15 +1,17 @@
 from Bio import Phylo
 from io import StringIO
 
+
 class GuidedAligning:
     def __init__(self, graph_list, guide_tree):
-        # convert tree into newick-format
-        handle = StringIO()
-        Phylo.write(guide_tree, handle, "newick")
-        gt_newickformat = handle.getvalue()
-
-
+        self.graph_list = graph_list
+        self.guide_tree = guide_tree
         pass
 
     def calc_aligning(self):
+        # convert tree into newick-format
+        handle = StringIO()
+        Phylo.write(self.guide_tree, handle, "newick")
+        gt_newickformat = handle.getvalue()
+
         pass
