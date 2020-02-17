be888f5793804d649f82631cd68d7d6deef91262
felixlinker <linkerfelix@gmail.com>
2020-02-17 14:43:11 +0100

    Add crop_graph

---
+++ trier/graph/crop.py
@@ -0,0 +1,25 @@
+import numpy as np
+from .adjacency import AdjacencyGraph, SubGraph
+from scipy.sparse.csgraph import dijkstra
+
+
+# This function maps a matrix returned by dijkstra to an adjacency matrix that
+# represents the transitive closure of the original matrix where there is an
+# edge between any reachable nodes
+_map_transitive_closure = np.vectorize(
+    lambda x: int(0 < x and x < float('inf')))
+
+
+def crop_graph(graph: AdjacencyGraph):
+    components = graph.get_tails() + graph.get_cycles()
+    new_adj = np.zeros((len(components), len(components)))
+    for comp, i in enumerate(components):
+        base = comp.adj_matrix + graph.adj_matrix
+        for other, j in enumerate(components[i + 1:]):
+            adj = base + other.adj_matrix
+            reachable_closure = _map_transitive_closure(
+                dijkstra(adj, directed=False))
+            new_adj[i][i + 1 + j] = int(np.any(adj < reachable_closure))
+    return AdjacencyGraph(gid=graph.id + '_reduced', matrix=new_adj,
+                          edge_labels=np.full(new_adj.shape, ''),
+                          node_labels=list(map(SubGraph.chem_toString, components)))
