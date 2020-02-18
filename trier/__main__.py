fccc791d79005eb86705384812e457abe2f4fe74
felixlinker <linkerfelix@gmail.com>
2020-02-18 16:35:02 +0100

    Add file output

--- trier/__main__.py
+++ trier/__main__.py
@@ -1,7 +1,11 @@
 from argparse import ArgumentParser
 from glob import glob
 from multivitamin.utils.parser import parse_graph
+from multivitamin.utils.graph_writer import write_graph
 from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
+import Bio.Phylo as Phylo
+from os import path
+from pathlib import Path
 
 from trier.scoring import Symmetric, ComponentScorer, CroppingScorer
 from trier.aligning import GuidedAligning
@@ -13,6 +17,7 @@ parser = ArgumentParser()
 parser.add_argument('--inputs', '-i', type=str, nargs='+', required=True)
 parser.add_argument('--scorer', '-s', choices=scorers.keys(),
                     default=Symmetric.__name__)
+parser.add_argument('--outdir', '-o', type=str, required=False)
 args = parser.parse_args()
 
 inputs = concat(map(glob, args.inputs))
@@ -24,3 +29,11 @@ tree = tree_constructor.upgma(scorer.get_scoring())
 aligner = GuidedAligning(graphs, tree)
 aligning = aligner.calc_aligning()
 print(aligning)
+
+if args.outdir:
+    Path(args.outdir).mkdir(parents=True, exist_ok=True)
+    with open(path.join(args.outdir, 'scoring.phylip'), 'w') as handle:
+        scorer.get_scoring().format_phylip(handle)
+    Phylo.write(tree, path.join(args.outdir, 'guide.newick'), 'newick')
+    for k, g in aligning.items():
+        write_graph(g, args.outdir, k)
