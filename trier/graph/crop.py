e102b1b29d2026d88609b635156dab3f3d9cd72a
felixlinker <linkerfelix@gmail.com>
2020-02-18 11:02:11 +0100

    Fix reachability check when cropping graphs

--- trier/graph/crop.py
+++ trier/graph/crop.py
@@ -14,9 +14,10 @@ def crop_graph(graph: AdjacencyGraph):
     components = graph.get_tails() + graph.get_cycles()
     new_adj = np.zeros((len(components), len(components)))
     for comp, i in enumerate(components):
-        base = comp.adj_matrix + graph.adj_matrix
+        base = _map_transitive_closure(
+            dijkstra(comp.adj_matrix + graph.adj_matrix, directed=False))
         for other, j in enumerate(components[i + 1:]):
-            adj = base + other.adj_matrix
+            adj = base + _map_transitive_closure(dijkstra(other.adj_matrix))
             reachable_closure = _map_transitive_closure(
                 dijkstra(adj, directed=False))
             new_adj[i][i + 1 + j] = int(np.any(adj < reachable_closure))
