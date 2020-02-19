7bb8896e1d449ab022a35e5a376b374e884463dc
roman-at-github <roman.matern@yahoo.de>
2020-02-19 17:15:51 +0100

    Felixs Idee umgesetzt

--- trier/aligning.py
+++ trier/aligning.py
@@ -1,13 +1,22 @@
 from Bio import Phylo
 from io import StringIO
 from multivitamin.algorithms.vf2_subsub import subVF2
+from functools import reduce
+
+
+def get_parent(tree, node):
+    path = tree.get_path(node)
+    # wenn Pfad die laenge 1 hat, ist die Wurzel der Parent-Knoten
+    if len(path) <= 1:
+        return tree.clade
+    else:
+        return path[-2]
 
 
 class GuidedAligning:
     def __init__(self, graph_list, guide_tree):
         self.graph_list = graph_list
         self.guide_tree = guide_tree
-        self.alignment_pairs = []
         self.parents = []
         self.results = {}
         for graph in graph_list:
@@ -18,68 +27,32 @@ class GuidedAligning:
 
         # Alle Eltern-Knoten, der Blaetter bestimmen
         for leaf in leafs:
-            leaf_path = self.guide_tree.get_path(leaf)
-            # wenn Pfad die laenge 1 hat, ist die Wurzel der Parent-Knoten
-            if len(leaf_path) == 1:
-                parent = self.guide_tree.clade
-            else:
-                parent = leaf_path[-2]
-            self.parents.append(parent)
+            self.parents.append(get_parent(self.guide_tree, leaf))
 
-        # Wenn zwei Eltern-Knoten uebereinstimmen, sollen ihr Kinder-Knoten aligniert werden.
-        for p1 in range(0, len(self.parents)-1):
-            for p2 in range(p1+1, len(self.parents)):
-                if self.parents[p1] == self.parents[p2]:
-                    #graphen bestimmen
-                    g1 = self.parents[p1].clades[0].name
-                    g2 = self.parents[p1].clades[1].name
-                    # Aligniere Paar mit subVF2
-                    instance = subVF2(self.results[g1], self.results[g2])
-                    instance.match()
-                    result_graph = instance.get_real_result_graph()
-                    # Speichere Result in dict und alignment_pairs
-                    self.alignment_pairs.append(self.parents[p1])
-                    self.results[self.parents[p1].name] = result_graph
-
-        self.rec(self.alignment_pairs)
+        self.rec()
         return self.results
 
-    def rec(self, list_of_pairs):
-        test_parent = []
-        # wieder nach Eltern-Knoten schauen
-        for p in list_of_pairs:
-            leaf_path = self.guide_tree.get_path(p)
-            if len(leaf_path) <= 1:
-                parent = self.guide_tree.clade
-            else:
-                parent = leaf_path[-2]
-            test_parent.append(parent)
-
+    def rec(self):
         new_parents = []
-        test_root = self.guide_tree.clade
-        # wieder nach Ubereinstimmungen schauen, damit Kinder-Knoten in alignier-Liste aufgenommen werden
-        for p1 in test_parent:
-            matched = False
-            for p2 in range(0, len(self.parents)):
-                if p1 == self.parents[p2]:
-                    # graphen bestimmen
-                    g1 = p1.clades[0].name
-                    g2 = p1.clades[1].name
-                    # Aligniere Paar mit subVF2
-                    instance = subVF2(self.results[g1], self.results[g2])
-                    instance.match()
-                    result_graph = instance.get_real_result_graph()
-                    # Speichere Result in dict und alignment_pairs
-                    new_parents.append(p1)
-                    self.results[p1.name] = result_graph
-                    # testen ob es sich bereits um die Wurzel des Alignmentbaums handelt
-                    test_root = p1
-                    matched = True
-            if matched is False and p1 not in self.parents:
-                self.parents.append(p1)
+        # wieder nach Eltern-Knoten schauen
+        for p in self.parents:
+            all_leafs_in_result = True
+            for leaf in p.clades:
+                if leaf.name not in self.results:
+                    all_leafs_in_result = False
+                    break
+            if p.name not in self.results:
+                if all_leafs_in_result:
+                    self.results[p.name] = reduce(self.aggregator, p.clades[1:], self.results[p.clades[0].name])
+                    new_parents.append(get_parent(self.guide_tree, p))
+                else:
+                    new_parents.append(p)
 
-        # wenn beide Eltern-Knoten die Wurzel des baums sind, ist Rekursion beendet
-        if test_root == self.guide_tree.clade:# and test_root2 == self.guide_tree.clade:
-            return
-        else:
-            self.rec(new_parents)
+        if len(new_parents):
+            self.parents = new_parents
+            self.rec()
+
+    def aggregator(self, aggr, v):
+        instance = subVF2(aggr, self.results[v.name])
+        instance.match()
+        return instance.get_real_result_graph()
