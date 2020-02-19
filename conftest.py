8cb449f5bd4ca345ac4f465dc15d89996410c44a
felixlinker <linkerfelix@gmail.com>
2020-02-19 14:20:43 +0100

    Load config json statically

--- conftest.py
+++ conftest.py
@@ -1,13 +1,10 @@
 import json
 from multivitamin.utils.parser import parse_graph
 
-def pytest_addoption(parser):
-    parser.addoption("--cfg", required=True)
-
 
 def pytest_generate_tests(metafunc):
     cfg = None
-    with open(metafunc.config.getoption('cfg')) as fp:
+    with open('./tests.json') as fp:
         cfg = json.load(fp)
     for graph_cfg in cfg['graphs']:
         graph_cfg['mv_graph'] = parse_graph(graph_cfg['mv_graph'])
