d85d51f7faa2fe0127b03e40a2c162f9d38dce96
felixlinker <linkerfelix@gmail.com>
2020-02-19 15:31:14 +0100

    Test number of edges in cropped graph

--- trier/tests/graph/test_crop.py
+++ trier/tests/graph/test_crop.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from .util import set_up
 from ...graph.crop import crop_graph
 
@@ -7,3 +9,4 @@ def test_crop(graph):
     cropped = crop_graph(g)
     nc = graph['cropped_nodes']
     assert cropped.adj_matrix.shape == (nc, nc)
+    assert np.sum(np.tril(cropped.adj_matrix)) == graph['cropped_edges']
