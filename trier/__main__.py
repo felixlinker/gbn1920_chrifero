b6b3a24964f5e99f35392dfb4890bd89f4750661
felixlinker <linkerfelix@gmail.com>
2020-02-20 12:20:06 +0100

    Add option to pass guide tree in as cmd line argument

--- trier/__main__.py
+++ trier/__main__.py
@@ -20,21 +20,27 @@ parser.add_argument('--scorer', '-s', choices=scorers.keys(),
                     default=Symmetric.__name__)
 parser.add_argument('--outdir', '-o', type=str, required=False)
 parser.add_argument('--no-alignment', '-A', action='store_true')
+parser.add_argument('--tree', '-t', type=str, required=False, default=None)
 args = parser.parse_args()
 
 inputs = concat(map(glob, args.inputs))
 graphs = list(map(parse_graph, inputs))
-scorer = scorers[args.scorer](graphs)
-scorer.calc_scoring()
-distance = score_to_distance(scorer.get_scoring())
-tree_constructor = DistanceTreeConstructor()
-tree = tree_constructor.upgma(distance)
 
-if args.outdir:
-    Path(args.outdir).mkdir(parents=True, exist_ok=True)
-    with open(path.join(args.outdir, 'distance.phylip'), 'w') as handle:
-        distance.format_phylip(handle)
-    Phylo.write(tree, path.join(args.outdir, 'guide.newick'), 'newick')
+tree = None
+if args.tree is None:
+    scorer = scorers[args.scorer](graphs)
+    scorer.calc_scoring()
+    distance = score_to_distance(scorer.get_scoring())
+    tree_constructor = DistanceTreeConstructor()
+    tree = tree_constructor.upgma(distance)
+
+    if args.outdir:
+        Path(args.outdir).mkdir(parents=True, exist_ok=True)
+        with open(path.join(args.outdir, 'distance.phylip'), 'w') as handle:
+            distance.format_phylip(handle)
+        Phylo.write(tree, path.join(args.outdir, 'guide.newick'), 'newick')
+else:
+    tree = next(Phylo.parse(args.tree, args.tree.split('.')[-1]))
 
 if not args.no_alignment:
     aligner = GuidedAligning(graphs, tree)
